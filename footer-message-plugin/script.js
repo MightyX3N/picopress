8688eba476dac0049f5395415153b4f10b2e97a1
MightyX3N <115364527+MightyX3N@users.noreply.github.com>
2025-08-24 16:23:56 +0200

    Delete footer-message-plugin directory

--- footer-message-plugin/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const msg = document.getElementById('footer-msg');
-    if (!msg) return;
-    msg.addEventListener('click', () => msg.classList.toggle('clicked'));
-});
