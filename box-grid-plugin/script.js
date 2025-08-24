febbfd482811f27017caa11b0c2e9d29368505e7
MightyX3N <115364527+MightyX3N@users.noreply.github.com>
2025-08-24 16:24:10 +0200

    Delete box-grid-plugin directory

--- box-grid-plugin/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.grid-box').forEach(box => {
-        box.addEventListener('click', () => box.classList.toggle('clicked'));
-    });
-});
