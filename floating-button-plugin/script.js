bf5de39a809ce11108712816ade3a008be83c862
MightyX3N <115364527+MightyX3N@users.noreply.github.com>
2025-08-24 16:24:03 +0200

    Delete floating-button-plugin directory

--- floating-button-plugin/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const btn = document.getElementById('floating-btn');
-    if (!btn) return;
-    btn.addEventListener('click', () => btn.classList.toggle('clicked'));
-});
