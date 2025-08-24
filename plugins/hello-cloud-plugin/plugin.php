183b3ecf5cf2fe8b336ce520082061b1361a2ace
MightyX3N <115364527+MightyX3N@users.noreply.github.com>
2025-08-24 15:30:54 +0200

    Create plugin.php

---
+++ plugins/hello-cloud-plugin/plugin.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Hello Cloud Plugin
+Description: A simple cloud plugin that adds a [hello_cloud] shortcode
+Version: 1.0.0
+Author: xAI
+*/
+
+// Prevent direct access
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+// Register shortcode
+add_shortcode('hello_cloud', function($atts) {
+    $atts = shortcode_atts(['name' => 'World'], $atts);
+    return '<div class="hello-cloud">' . esc_html__('Hello, ' . $atts['name'] . '!', 'hello-cloud') . '</div>';
+});
+
+// Add translation support
+add_action('init', function() {
+    load_plugin_textdomain('hello-cloud', false, dirname(plugin_basename(__FILE__)) . '/languages');
+});
+
+?>
